5cbb4ca232dc3c33aaebf0d74fd9fd3d61901256
AlvaroGarSe <alu.140009@usj.es>
2024-05-16 01:15:44 +0200

    pauseMenu

---
+++ Assets/Scripts/UIScripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    public GameObject pauseMainMenu;
+    public GameObject pauseOptionsMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }else 
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMainMenu.SetActive(true);
+        pauseOptionsMenu.SetActive(false);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+
+    }
+
+    public void Pause()
+    {
+        pauseMainMenu.SetActive(true);
+        pauseOptionsMenu.SetActive(false);
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

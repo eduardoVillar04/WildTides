3d664ce3edaa76deb60c1a30b4ec500af5d3297e
AlvaroGarSe <132345019+AlvaroGarSe@users.noreply.github.com>
2024-05-16 18:15:00 +0200

    compass

---
+++ Assets/Scripts/UIScripts/CompassController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassController : MonoBehaviour
+{
+    public RectTransform compassBarTransform;
+
+    public RectTransform objectiveMarkerTransform;
+    public RectTransform northMarkerTransform;
+    public RectTransform southMarkerTransform;
+    public RectTransform eastMarkerTransform;
+    public RectTransform westMarkerTransform;
+
+    public Transform cameraObjectTransform;
+    public Transform objectiveObjectTransform;
+
+    public Camera mainCam;
+    public GameObject pauseMenu;
+    
+    void Update()
+    {
+        SetMarkerPosition(objectiveMarkerTransform, objectiveObjectTransform.position);
+        SetMarkerPosition(northMarkerTransform, Vector3.forward * 1000);
+        SetMarkerPosition(southMarkerTransform, Vector3.back * 1000);
+        SetMarkerPosition(eastMarkerTransform, Vector3.right * 1000);
+        SetMarkerPosition(westMarkerTransform, Vector3.left * 1000);
+
+        //We disable the compass bar when the player aims
+        if (!mainCam.gameObject.activeSelf || pauseMenu.activeSelf)
+        {
+            compassBarTransform.gameObject.SetActive(false);
+        } else {compassBarTransform.gameObject.SetActive(true); }
+
+
+    }
+
+    private void SetMarkerPosition(RectTransform markerTransform, Vector3 worldPosition)
+    {
+        Vector3 directionToTarget = worldPosition - cameraObjectTransform.position;
+        float angle = Vector2.SignedAngle(new Vector2(directionToTarget.x, directionToTarget.z),new Vector2(cameraObjectTransform.transform.forward.x, cameraObjectTransform.transform.forward.z));
+        float compassPositionX = Mathf.Clamp(2 * angle / mainCam.fieldOfView, -1, 1);
+        markerTransform.anchoredPosition = new Vector2(compassBarTransform.rect.width / 2 * compassPositionX, 0);
+    }
+}

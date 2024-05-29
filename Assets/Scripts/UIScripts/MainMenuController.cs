e864129ea745c61cdab5cb913ccab59dc39c783c
Urko Aguillo <118058363+Urkelele@users.noreply.github.com>
2024-05-29 20:08:27 +0200

    DecoracionPuertos+AlgunCambio

--- Assets/Scripts/UIScripts/MainMenuController.cs
+++ Assets/Scripts/UIScripts/MainMenuController.cs
@@ -8,27 +8,18 @@ public class MainMenuController : MonoBehaviour
 {
     public GameObject firstButton;
 
-    [Header("AUDIO")]
-    public AudioClip m_ClickSound;
-
     private void OnEnable()
     {
-        //Audio
-        SoundEffectsManager.instance.PlaySoundFXClip(m_ClickSound, transform, 0.99f);
         EventSystem.current.SetSelectedGameObject(firstButton);
     }
 
     public void PlayGame ()
     {
-        //Audio
-        SoundEffectsManager.instance.PlaySoundFXClip(m_ClickSound, transform, 0.99f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame ()
     {
-        //Audio
-        SoundEffectsManager.instance.PlaySoundFXClip(m_ClickSound, transform, 0.99f);
         Debug.Log("Quit");  
         Application.Quit();
     }
